ba7c5620d9f1140dc8e6948253ec7d13d7870abc
Caroline Costa Saldanha <125291901+carolsadn@users.noreply.github.com>
2023-12-04 17:16:25 -0300

    Ajustes na tela de assentos

--- PWNB-Intercine/js/script.js
+++ PWNB-Intercine/js/script.js
@@ -12,3 +12,36 @@ document.addEventListener("DOMContentLoaded", function() {
         emCartazLink.classList.remove("active");
     });
 });
+
+// para gerar grade na pagina de assentos
+const assentosContainer = document.getElementById('assentos-container');
+const numeroDeAssentosPorLado = 14;
+const numeroDeLinhas = 13;
+const letrasFileira = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+function criarAssentos() {
+    for (let linha = 0; linha < numeroDeLinhas; linha++) {
+        for (let assento = 1; assento <= numeroDeAssentosPorLado; assento++) {
+            const assentoElement = document.createElement('div');
+            assentoElement.classList.add('assento');
+            
+            // Adiciona a classe 'lateral' para os assentos laterais
+            if (assento > numeroDeAssentosPorLado / 2) {
+                assentoElement.classList.add('lateral');
+            }
+
+            const letraFileira = letrasFileira.charAt(linha);
+            assentoElement.textContent = `${letraFileira}${assento}`;
+            assentoElement.addEventListener('click', () => reservarAssento(assentoElement));
+            assentosContainer.appendChild(assentoElement);
+        }
+    }
+}
+
+function reservarAssento(assentoElement) {
+    assentoElement.classList.toggle('reservado');
+}
+
+criarAssentos();
+
+
